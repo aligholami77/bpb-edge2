be6da533e295182d345765b2b64147a7b0729420
aligholami77 <126642039+aligholami77@users.noreply.github.com>
2026-06-01 15:32:01 +0330

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 const TARGET = 'https://holy-silence-fb62.ali-dd6.workers.dev/eyJqdW5rIjoiYzZYblkxN0drIiwicHJvdG9jb2wiOiJ2bCIsIm1vZGUiOiJwcm94eWlwIiwicGFuZWxJUHMiOlsicHlpcC55Z2tray5kcGRucy5vcmciXX0=?ed=2560';
 
 app.use('/', async (req, res) => {
